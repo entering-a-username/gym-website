c17fa87ef0e73b9b9bc41fa61e9cdcc7b856ab8e
entering-a-username <tamunacer@gmail.com>
2023-06-07 20:16:10 +0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     extend: {
       zIndex: {
         '70': '70',
-      }
+      },
+    },
+    
+    screens: {
+      'smsm': '500px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [],
